db41d3eadb61a6567085dcc4e5d390880705518a
Debjit Das <debjit@binod.binod>
2025-05-09 13:45:41 +0530

    Backend folder remove

--- Backend/services/user.service.js
+++
@@ -1,8 +0,0 @@
-const userModel = require('../models/user.model');
-
-module.exports.createUser = async({name,email,password,forestId})=>{
-    const hashedPassword = await userModel.hashPassword(password);
-    const user = await userModel.create({name,email,password:hashedPassword,forestId});
-    return user;
-}
-
